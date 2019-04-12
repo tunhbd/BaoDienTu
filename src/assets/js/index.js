24678f9575e4c521a82dd787697047136c40b686
Vz <phhoangviet.hoctap@gmail.com>
2019-04-12 09:25:22 +0700

    index 30%

---
+++ src/assets/js/index.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+  $('.slick-image').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    infinite: true,
+    dots:false,
+    arrows:false,
+    autoplaySpeed: 2500,
+  });
+});
+$(document).ready(function(){
+  $('.slick-new-image').slick({
+    
+   vertical: true,
+   slidesToShow: 2,
+   slidesToScroll: 2,
+   verticalSwiping: true,
+   arrows:false,
+   infinite: false,
+   rtl:false,
+     // accessibility: false,
+  });
+});
+$('.slick-new-image').on('wheel', (function(e) {
+  e.preventDefault();
+
+  if (e.originalEvent.deltaY > 0) {
+    $(this).slick('slickNext');
+  } else {
+    $(this).slick('slickPrev');
+  }
+}));
