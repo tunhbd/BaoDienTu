f332369e9ee13bd75bf6302d6a6ed5fd734025a6
wi <letuthptnguyendueduvn@gmail.com>
2019-04-21 22:54:09 -0400

    login split into 3 pages

--- src/assets/js/auth.js
+++ src/assets/js/auth.js
@@ -4,17 +4,8 @@
   let frmCtrl = a(".pwd-mask > .form-control");
   let tglPwd = a(".pwd-toggle");
 
-  a(".lnk-toggler").on("click", function(event) {
-    event.preventDefault();
-
-    let dtPanel = a(this).data("panel");
-    a(".w-panel.active").removeClass("active");
-    a(dtPanel).addClass("active");
-  });
-
   a(tglPwd).on("click", function(event) {
     event.preventDefault();
-    console.log(1)
     a(this).toggleClass("fa-eye-slash fa-eye");
     a(this).hasClass("fa-eye")
       ? a(frmCtrl).attr("type", "text")
